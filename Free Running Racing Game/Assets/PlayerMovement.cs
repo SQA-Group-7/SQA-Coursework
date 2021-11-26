9fd0dd0b30c39486a195cb6b25be882e87cfc1ce
Tim Bartrum <timbartrum73@gmail.com>
2021-11-26 20:06:28 +0000

    Updated movement to use GetAxis

--- Free Running Racing Game/Assets/PlayerMovement.cs
+++ Free Running Racing Game/Assets/PlayerMovement.cs
@@ -8,38 +8,43 @@ public class PlayerMovement : MonoBehaviour
     public float Speed;
     public float JumpPower;
 
-    private Rigidbody rb;
+    private bool _jumpPressed;
+    private float _horizontalInput;
+    private float _verticalInput;
+    private float _zAxisInput;
 
+    private Rigidbody _rigidBody;
+
+
+    // Start is called before the first frame update - initialize stuff here
     void Start()
     {
-          rb = GetComponent<Rigidbody>();
+        _rigidBody = GetComponent<Rigidbody>();
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _jumpPressed = true;
+        }
+
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
+    }
+
+    // FixedUpdate is called once every physics update (100 times per second - 100hz default)
     void FixedUpdate()
     {
-        if (Input.GetKey("w"))
+
+        _rigidBody.velocity = new Vector3(_horizontalInput * Speed, _rigidBody.velocity.y, _verticalInput * Speed);
+
+        if (_jumpPressed)
         {
-            transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+            _rigidBody.AddForce(Vector3.up * JumpPower, ForceMode.VelocityChange);
+            _jumpPressed = false;
         }
 
-        else if (Input.GetKey("s"))
-        {
-            transform.Translate(Vector3.back * Speed * Time.deltaTime);
-        }
-
-        if (Input.GetKey("a"))
-        {
-            transform.Translate(Vector3.left * Speed * Time.deltaTime);
-        }
-
-        else if (Input.GetKey("d"))
-        {
-            transform.Translate(Vector3.right * Speed * Time.deltaTime);
-        }
-
-        if (Input.GetKey("space"))
-        {
-            rb.AddForce(Vector3.up * JumpPower);
-        }
     }
 }
