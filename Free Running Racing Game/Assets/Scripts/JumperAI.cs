0fe6c9ba7b91374be359d7ce267d2bb2b8e4e4c4
JacquesJajo <jacques2000.x3@gmail.com>
2021-12-09 13:32:40 +0000

    Force Collisions

--- Free Running Racing Game/Assets/Scripts/JumperAI.cs
+++ Free Running Racing Game/Assets/Scripts/JumperAI.cs
@@ -12,16 +12,26 @@ public class JumperAI : MonoBehaviour
     public Transform[] Players;
     public float AgroRange;
     public float Speed;
+    public float RoamRange;
+    public float PushForce;
 
     private State _currentState = State.IDLE;
     private Vector3 _target;
     private Rigidbody _rigidbody;
 
     private List<float> _distances = new List<float>();
+    private Vector3 _startPosition;
 
     void Start()
     {
+        _startPosition = transform.position;
         _rigidbody = GetComponent<Rigidbody>();
+
+        _target = new Vector3();
+        // Code reuse metric hey ho
+        _target.x = Random.Range(_startPosition.x - RoamRange, _startPosition.x + RoamRange);
+        _target.y = _startPosition.y;
+        _target.z = Random.Range(_startPosition.z - RoamRange, _startPosition.z + RoamRange);
     }
 
     void Update()
@@ -54,6 +64,16 @@ public class JumperAI : MonoBehaviour
                 _rigidbody.velocity = Vector3.zero;
             }
         }
+
+        else if (_currentState == State.IDLE)
+        {
+            if (transform.position == _target)
+            {
+                _target.x = Random.Range(_startPosition.x - RoamRange, _startPosition.x + RoamRange);
+                _target.y = _startPosition.y;
+                _target.z = Random.Range(_startPosition.z - RoamRange, _startPosition.z + RoamRange);
+            }
+        }
     }
 
     void FixedUpdate()
@@ -62,6 +82,21 @@ public class JumperAI : MonoBehaviour
         {
             Vector3 toTarget = _target - transform.position;
             _rigidbody.AddForce(toTarget * Speed);
+        }
+
+        else if (_currentState == State.IDLE)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _target, Speed * Time.deltaTime);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Rigidbody playerBody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 toCollision = collision.gameObject.transform.position - transform.position;
+            playerBody.AddForce(toCollision * PushForce);
         }
     }
 }
